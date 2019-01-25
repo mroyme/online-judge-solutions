06a39d2afaf4668b2cdeb92f35bf08fd41944033
Madhurjya Roy <roymadhurjya@gmail.com>
2019-01-25 15:14:03 +0530

    add SALARY

---
+++ codechef/SALARY.c
@@ -0,0 +1,29 @@
+/**
+ * Problem:     SALARY
+ * Author:      Madhurjya Roy
+ * Link:        https://www.codechef.com/problems/SALARY
+ * Environment: C (Gcc 6.3)
+ */
+
+#include <stdio.h>
+#include <limits.h>
+
+int main()
+{
+	int t;
+	scanf("%d", &t);
+	while (t--) {
+		int n;
+		scanf("%d", &n);
+		int min = INT_MAX, sum = 0, i;
+		for (i = 0; i < n; i++) {
+			int tmp;
+			scanf("%d", &tmp);
+			sum += tmp;
+			if (tmp < min) {
+				min = tmp;
+			}
+		}
+		printf("%d\n", sum - n * min);
+	}
+}
