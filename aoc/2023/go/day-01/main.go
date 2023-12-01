09a4e05d32d677b460d51372c8590eeaeceaaef5
Madhurjya Roy <mroy@nzxt>
2023-12-01 21:53:28 +0530

    Add aoc-23 day-01 go

---
+++ aoc/2023/go/day-01/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"unicode"
+)
+
+func main() {
+	f, err := os.Open("input")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	sc := bufio.NewScanner(f)
+
+	sum := 0
+
+	for sc.Scan() {
+
+		first := 0
+		last := 0
+
+		line := []rune(sc.Text())
+
+		// find first
+		for i := range line {
+			c := line[i]
+			if unicode.IsDigit(c) {
+				first = int(c)
+				break
+			}
+		}
+
+		// find last
+		for i := range line {
+			c := line[len(line)-1-i]
+			if unicode.IsDigit(c) {
+				last = int(c)
+				break
+			}
+		}
+
+		sum += first*10 + last
+	}
+	fmt.Println(sum)
+}
---
+++ aoc/2023/go/go.mod
@@ -0,0 +1,3 @@
+module github.com/mroyme/online-judge-solutions/aoc/2023/go
+
+go 1.21.4
